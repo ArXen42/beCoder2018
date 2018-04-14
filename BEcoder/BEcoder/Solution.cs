fa68ee0db435c10541397d0f2544921c36b8985e
Georgii Pelageikin <arxen42@tutanota.com>
2018-04-14 14:32:16 +0300

    Possible solution

--- BEcoder/BEcoder/Solution.cs
+++ BEcoder/BEcoder/Solution.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BEcoder
 {
@@ -7,12 +9,31 @@ namespace BEcoder
 	{
 		public static void Solve(TextReader input, TextWriter output)
 		{
-			//Stub
+			String firstLine = input.ReadLine();
+			Int32 rootFriendsCount = Int32.Parse(firstLine);
 
-			for (Int32 i = 0; i < 5; i++)
-				input.ReadLine();
+			HashSet<Int32> rootFriends = new HashSet<Int32>();
+			HashSet<Int32> friends = new HashSet<Int32>();
 			
-			output.WriteLine("6");
+			for (Int32 i = 0; i < rootFriendsCount; i++)
+			{
+				// ReSharper disable once PossibleNullReferenceException
+				var rootFriendLine = input.ReadLine()
+					.Split(' ')
+					.Select(Int32.Parse)
+					.ToArray();
+
+				Int32 rootFriendId = rootFriendLine[0];
+				Int32 friendsCount = rootFriendLine[1];
+
+				rootFriends.Add(rootFriendId);
+				for (Int32 f = 0; f < friendsCount; f++)
+					friends.Add(rootFriendLine[f + 2]);
+			}
+			
+			friends.ExceptWith(rootFriends);
+			
+			output.WriteLine(friends.Count);
 		}
 	}
 }
